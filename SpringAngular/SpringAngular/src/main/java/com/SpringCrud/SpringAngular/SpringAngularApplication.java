7fb0c884103dc6c435bc3dc143fda8232cb760d6
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-10 19:36:37 +0530

    upadated initially

---
+++ SpringAngular/SpringAngular/src/main/java/com/SpringCrud/SpringAngular/SpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.SpringCrud.SpringAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularApplication.class, args);
+	}
+
+}
---
+++ SpringAngular/SpringAngular/src/test/java/com/SpringCrud/SpringAngular/SpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringCrud.SpringAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
