eb17285348e099308ff59ab87ec05d55d8047815
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-10 20:16:41 +0530

    Create CustomerController.java

---
+++ SpringAngular/SpringAngular/src/main/java/com/SpringCrud/SpringAngular/CustomerController/CustomerController.java
@@ -0,0 +1,17 @@
+package com.SpringCrud.SpringAngular.CustomerController;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController{
+
+
+
+
+
+
+
+
+
+}
